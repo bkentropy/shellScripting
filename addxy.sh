70923d79707f38cc59f2ee4fca74cf6acfbee23b
Brian Kustra <bkustrakb@gmail.com>
2016-02-02 13:23:47 -0600

    Modified addxy to throw some errors, added a present working directory script

--- addxy.sh
+++ addxy.sh
@@ -1,4 +1,15 @@
 #!/bin/bash
 read -p "Enter two numbers : " x y
+if [ $x -ge 0 ] ; then
+  echo "$x"
+else
+  echo "Enter a number!"
+fi
+
+if [ $y -ge 0 ] ; then
+  echo "$y"
+else
+  echo "Enter a number!"
+fi
 ans=$((x+y))
 echo "$x + $y = $ans"
